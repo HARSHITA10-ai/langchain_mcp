d9f60912c3ae5adc58e46910be5e07572661145d
HARSHITA AGGALA <harshita@slris.com>
2025-10-26 17:10:39 +0530

    Refactor FastAPI setup to include lifespan for agent initialization and remove unused endpoint

--- app.py
+++ app.py
@@ -2,26 +2,28 @@
 Main FastAPI Entry Point for the Multi-Agent Payroll Analysis System.
 Simplified: always returns JSON matching the Supervisor unified template.
 """
-import sys
-import os
-import base64
-import json
-import asyncio
 
 from client import main, initialize_agent
 from fastapi import FastAPI, HTTPException
+# from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import List, Dict, Any
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from mcp import ClientSession, StdioServerParameters
-from mcp.client.stdio import stdio_client
+from contextlib import asynccontextmanager
 
 # --- Load environment ---
 load_dotenv()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    await initialize_agent()
+    yield
+
+
 # --- FastAPI setup ---
-app = FastAPI(title="Payroll Analysis Multi-Agent API")
+app = FastAPI(title="Payroll Analysis Multi-Agent API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -49,38 +51,14 @@ class ChatResponse(BaseModel):
 def health_check():
     return {"status": "ok"}
 
-@app.get("/initialize_agent")
-async def agent_setup():
-   
-    try:
-         
-       
-
-        # Send the user's message to the agent
-        response = await initialize_agent()
-
-        if response:
-
-
-            return {
-                "content": "agent initialized"
-            }
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
 # --- Main chat endpoint ---
 @app.post("/chat", tags=["Chat"])
 async def chat_endpoint(request: ChatRequest):
    
     try:
          
-       
-
         # Send the user's message to the agent
         response = await main(request.user_id,request.query)
-
-
         return {
             "content": response
         }
@@ -88,3 +66,14 @@ async def chat_endpoint(request: ChatRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
+    # try:
+    #     async def event_generator():
+    #         # main() is async generator
+    #         async for chunk in main(request.user_id, request.query):
+    #             # data = {"content": chunk}
+    #             yield f"data: {json.dumps(chunk)}\n\n"
+
+    #     return StreamingResponse(event_generator(), media_type="text/event-stream")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
